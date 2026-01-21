7f1ece3b3bec5a0048ddc417a304f470c6cc8438
beesou777 <sbeesou@gmail.com>
2026-01-21 19:52:44 +0545

    fix: build issue

--- app/api/remote-jobs/route.ts
+++ app/api/remote-jobs/route.ts
@@ -6,9 +6,10 @@ export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
     const page = parseInt(searchParams.get("page") || "1");
     const limit = parseInt(searchParams.get("limit") || "21");
+    const search = searchParams.get("q") || "";
 
     try {
-        const data = await fetchRemoteJobsFromAPI(page, limit);
+        const data = await fetchRemoteJobsFromAPI(page, limit,search);
         return NextResponse.json(data);
     } catch (error: any) {
         console.error("Proxy error fetching remote jobs:", error);
